8291cdf0edc15993e27a92144a1609ef075e3e38
dmnot <tomatsuper1701@gmail.com>
2022-10-20 23:58:28 +0500

    Finished with constructor explanation and extends

--- src/ru/synergy/ConstructorTest/Main.java
+++ src/ru/synergy/ConstructorTest/Main.java
@@ -3,71 +3,28 @@ package ru.synergy.ConstructorTest;
 public class Main {
     public static final Integer MAGNIFIER = 10000; // создание константы увеличивающее в 10000 раз
     public static void main(String[] args) {
-        Person person = new Person("Andrey",20);
-        person.displayName();
-        person.displayAge();
-        Person.getCounter();
-        Fraction fraction = new Fraction(10,0); // Создание объекта класса Fraction
+        //---------------------------------------------------------------------
+        Fraction fraction = new Fraction(10,45); // Создание объекта класса Fraction
+        fraction.addNumerator().addNumerator().addNumerator().addNumerator();
+        //---------------------------------------------------------------------
+        //Создание объекта класса Person
+        Person oleg = new Person("Oleg","Olegov",'m');
+        System.out.println(oleg);
+        //------------------------------------------------------------------------
+        // Создание объекта класса Employee
+        Employee olga = new Employee("Olga","Svetova",'f',"Buhgalter");
+
+        System.out.println(olga + "\n");
+        //---------------------------------
+        Person olga2 = olga; // Говорим Яве что объект класса Employee является объектом класса Person
+        System.out.println(olga2);
+        ((Employee) olga2).WhoAreU(); // Использование метода из класса Employee в объекте класса Person ((Employee) olga2)  - Говорим Яве что объект класса Person
+        // является объектом класса Employee
+        //---------------------------------
     }
 }
 // Создание класса
-//---------------------------------------------------------------
-class Person{
-    //Объявление переменных
-      int id;
-      String name;
-      protected int age;
-     private static int counter =0; // Создание счетчика
-    //-----------------------------------------------------------------------
-    //Создание метода для взятия значения
-    public static int getCounter() {
-        return counter;
-    }
-    //--------------------------------------------------
-    //Статическая инициализация (примечание для самого себя все переменные,методы которые статичны т.е.
-    // имеют слово static пред объявлением будут прогружены первыми вместе с классом перед выполнением программы
-    // а также в последующей инициализации и в будующих проектах то что входит в static должно быть объявлено статичным)
-        static {
-        System.out.println(counter);
-    }
-public Person(String name,int age){
-        id = counter++;
-        this.name = name;
-        this.age = age;
-
-    }
-
-    // Методы для вывода значений
-    //---------------------------
-    public void  displayName(){
-        System.out.printf("Имя: %s\n", name);
-    }
-    public void  displayAge(){
-        System.out.printf("Возраст: %d\n", age);
-    }
-    //---------------------------
-    // Методы для взятия значений
-    //-------------------------
-    public String getName() {
-        return name;
-    }
-
-
-    public int getAge() {
-        return age;
-    }
-   public void setName(String name){
-       this.name = name;
-    }
-
-    public void setAge(int age) {
-        if (age>0 && age<110)
-        this.age = age;
-    }
-    //---------------------------
-    //---------------------------------------------------------------
-}
-//Создание класса для конструктора
+// Создание класса для конструктора
 class Fraction{
     int numerator;
     int denumerator = 1;
@@ -82,4 +39,92 @@ class Fraction{
         this.denumerator = denumerator;
     }
     //------------------------------------------
+    public Fraction addNumerator(){
+        this.numerator++;
+        return this;
+    }
+     public Fraction getCuurentObject(){ return this;}
 }
+// Создание класса Person
+class Person{
+    //Объявление переменных
+   protected String firstName;
+    protected String Last_Name;
+    protected char gender;
+    //----------------------
+    //Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLast_Name() {
+        return Last_Name;
+    }
+
+    public void setLast_Name(String last_Name) {
+        Last_Name = last_Name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    //------------------------------
+    public Person(){
+        this("","",'-');
+    }
+    // Создание конструктора
+    public Person(String firstName, String Last_Name, char gender){
+        this(firstName, Last_Name);
+        this.gender = gender;
+    }
+    //Конвертация в строку для корректного вывода
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", Last_Name='" + Last_Name + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+    //----------------------------------------------
+
+    public Person(String firstName, String Last_Name){
+         this.firstName = firstName;
+         this.Last_Name = Last_Name;
+    }
+    //---------------------------------
+
+}
+
+//Создание класса для наследования
+class Employee extends Person{
+    protected  String dolznost;
+
+    public Employee(String firstName, String Last_Name, char gender, String dolznost) {
+        super(firstName, Last_Name, gender);
+        this.dolznost = dolznost;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", Last_Name='" + Last_Name + '\'' +
+                ", gender=" + gender + '\'' +
+                ",doznnost=" + dolznost +
+                '}';
+    }
+   public void WhoAreU(){
+        System.out.println("Hi my  class is:>>> " + this.getClass()); // Метод для отображения класса
+    }
+}
+
